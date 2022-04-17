65dabef1a62c5852fad432cbf717cb6b1cbd4510
monish karunakaran <ckmonish2000@gmail.com>
2022-04-17 21:41:41 +0530

    read more or less

--- components/DetailsDesc.js
+++ components/DetailsDesc.js
@@ -1,15 +1,41 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Touchable } from 'react-native'
 import React, { useState } from 'react'
 
 import { ETHPrice, NFTTitle } from "../components/SubInfo"
-import { SIZES } from '../constants'
+import { SIZES, FONTS } from '../constants'
+import { COLORS } from './../constants/theme';
 
 export default function DetailsDesc({ data }) {
+  const [text, settext] = useState(data.description.slice(0, 200))
+  const [readmore, setreadmore] = useState(false)
   return (
-    <View style={style.root}>
-      <NFTTitle title={data.name} subTitle={data.creator} titleSize={SIZES.extraLarge} subTitleSize={SIZES.font} />
-      <View style={{ paddingHorizontal: SIZES.font }}>
-        <ETHPrice price={data.price} />
+    <View>
+      <View style={style.root}>
+        <NFTTitle title={data.name} subTitle={data.creator} titleSize={SIZES.extraLarge} subTitleSize={SIZES.font} />
+        <View style={{ paddingHorizontal: SIZES.font }}>
+          <ETHPrice price={data.price} />
+        </View>
+      </View>
+
+      <View style={style.desc}>
+        <Text style={{ fontSize: SIZES.font, fontFamily: FONTS.semiBold, color: COLORS.primary }}>Description</Text>
+      </View>
+
+      <View style={{ padding: SIZES.font }}>
+        <Text style={{ marginTop: SIZES.base, fontSize: SIZES.font, fontFamily: FONTS.regular, color: COLORS.secondary, lineHeight: SIZES.large }}>
+          {text}{!readmore ? "..." : ""}
+          {!readmore ?
+
+            <Text
+              onPress={() => { setreadmore(true); settext(data.description) }}
+              style={{ marginLeft: 10, fontSize: SIZES.medium, fontFamily: FONTS.semiBold, color: COLORS.primary }}>Read More</Text>
+            :
+            <Text
+              onPress={() => { setreadmore(false); settext(data.description.slice(0, 250)) }}
+              style={{ marginLeft: 10, fontSize: SIZES.medium, fontFamily: FONTS.semiBold, color: COLORS.primary }}>Show less</Text>
+
+          }
+        </Text>
       </View>
     </View>
   )
@@ -17,5 +43,12 @@ export default function DetailsDesc({ data }) {
 
 const style = StyleSheet.create({
   root: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  desc: {
+    paddingHorizontal: SIZES.font,
   }
 })
