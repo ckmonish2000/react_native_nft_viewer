2d1650284162f57a488b3a4d3af66ffafa4ba64d
monish karunakaran <ckmonish2000@gmail.com>
2022-04-17 21:25:08 +0530

    minor

--- components/DetailsDesc.js
+++ components/DetailsDesc.js
@@ -2,17 +2,20 @@ import { View, Text, StyleSheet } from 'react-native'
 import React, { useState } from 'react'
 
 import { ETHPrice, NFTTitle } from "../components/SubInfo"
+import { SIZES } from '../constants'
 
 export default function DetailsDesc({ data }) {
   return (
     <View style={style.root}>
-      <NFTTitle title={data.name} subTitle={data.creator} />
+      <NFTTitle title={data.name} subTitle={data.creator} titleSize={SIZES.extraLarge} subTitleSize={SIZES.font} />
+      <View style={{ paddingHorizontal: SIZES.font }}>
+        <ETHPrice price={data.price} />
+      </View>
     </View>
   )
 }
 
 const style = StyleSheet.create({
   root: {
-    marginTop: 20
   }
 })
